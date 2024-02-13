46d1e911d731d73a68f8c7a99c1d74004f776f0a
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2024-02-13 18:29:48 +0100

    Update: 📖 Add classrom schedule 📖

--- src/main/java/com/example/timestorm/HelloApplication.java
+++ src/main/java/com/example/timestorm/HelloApplication.java
@@ -1,19 +1,12 @@
 package com.example.timestorm;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class HelloApplication extends Application {
 
@@ -32,7 +25,12 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) throws IOException {
         user = new LoginProvider();
-        System.out.println("Token:" + user.getTokenExecution());
+        boolean isLogged = user.tryLogin("uapv2001785", "zujja7-rojsUt-wixduj");
+        if(isLogged){
+            System.out.println("User connected");
+        } else {
+            System.out.println("User not connected");
+        }
         launch();
     }
 }
--- src/main/java/com/example/timestorm/LoginProvider.java
+++ src/main/java/com/example/timestorm/LoginProvider.java
@@ -7,22 +7,29 @@ import org.jsoup.select.Elements;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class LoginProvider {
     private String tokenExecution;
+    private boolean isTeacher;
 
     public LoginProvider() throws IOException {
         if(! setTokenExecution()){
             System.out.println("Unable to login!");
         }
+        isTeacher = false;
     }
 
-    public String getTokenExecution() {
-        return tokenExecution;
-    }
-
+    /**
+     * Find the excution token of the university connection portal
+     * @param con
+     * @return tokenExecution
+     * @throws IOException
+     */
     private String extractExecutionValue(HttpURLConnection con) throws IOException {
         String htmlResponse = readResponse(con);
         Document doc = Jsoup.parse(htmlResponse);
@@ -31,6 +38,12 @@ public class LoginProvider {
         return executionInput.get(0) != null ? executionInput.get(0).attr("value") : null;
     }
 
+    /**
+     * Read Http response, and return it in a string
+     * @param con
+     * @return Http response in a string
+     * @throws IOException
+     */
     private String readResponse(HttpURLConnection con) throws IOException {
         StringBuilder response = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
@@ -42,6 +55,11 @@ public class LoginProvider {
         return response.toString();
     }
 
+    /**
+     * Set a new execution token
+     * @return true if a new one has been set
+     * @throws IOException
+     */
     private boolean setTokenExecution() throws IOException {
         URL obj = new URL("https://cas.univ-avignon.fr/cas/login?service=https%3A%2F%2Fedt.univ-avignon.fr%2Flogin");
 
@@ -60,7 +78,47 @@ public class LoginProvider {
         return false;
     }
 
+    public boolean tryLogin(String username, String password) throws IOException {
+        if (!username.contains("uapv")) {
+            System.out.println("Bad username format!");
+            return false;
+        }
+        /*if (!mail.contains("alumni")) {
+            isTeacher = true;
+        }*/
 
+        URL url = new URL("https://cas.univ-avignon.fr/cas/login?service=https%3A%2F%2Fedt.univ-avignon.fr%2Flogin");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "Java client");
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
+        String urlParameters = "username=" + URLEncoder.encode(username, "UTF-8") +
+                "&password=" + URLEncoder.encode(password, "UTF-8") +
+                "&_eventId=submit" +
+                "&execution=" + URLEncoder.encode(tokenExecution, "UTF-8");
+
+        con.setDoOutput(true);
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = urlParameters.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+
+        int responseCode = con.getResponseCode();
+        System.out.println("POST Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            String response = readResponse(con);
+            System.out.println(response);
+
+            // TODO: Read local Session Storage
+            return true;
+        } else {
+            System.out.println("POST request not worked");
+        }
+
+        return false;
+    }
 }
+
